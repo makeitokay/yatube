a12237ada94caa79606d129c199fac4ecf9629d7
makeitokay <andrey.mu11@yandex.ru>
2020-01-28 20:54:26 +0500

    auth: signup, signin, logout

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -13,10 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("posts.urls"))
+        # обработчик для главной страницы ищем в urls.py приложения posts
+        path("", include("posts.urls")),
+
+        # регистрация и авторизация
+        path("auth/", include("users.urls")),
+
+        # если нужного шаблона для /auth не нашлось в файле users.urls —
+        # ищем совпадения в файле django.contrib.auth.urls
+        path("auth/", include("django.contrib.auth.urls")),
+
+        # раздел администратора
+        path("admin/", admin.site.urls),
 ]
