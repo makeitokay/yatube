df9501df343f466e23e230cc04392b122deaa4fd
makeitokay <andrey.mu11@yandex.ru>
2020-01-29 19:43:01 +0500

    html: change and reset password, login, logout

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "index"
+# LOGOUT_REDIRECT_URL = "index"
