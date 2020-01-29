9e0e90caf6f041f476da9a1afc164f261c62ed89
makeitokay <andrey.mu11@yandex.ru>
2020-01-29 20:13:33 +0500

    login required pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,9 @@
 from .models import Post
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
     return render(request, "index.html", {"posts": latest})
