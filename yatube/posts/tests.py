8cb7f0714495cff46999c5304edd622b31299819
makeitokay <andrey.mu11@yandex.ru>
2020-02-20 16:30:29 +0500

    Separate testcases

--- yatube/posts/tests.py
+++ yatube/posts/tests.py
@@ -1,15 +1,15 @@
 from django.core import mail
 from django.test import TestCase, Client
 
-from .models import User
+from .models import User, Post
 
 
 class EmailTest(TestCase):
     def setUp(self) -> None:
-        self.client = Client()
+        client = Client()
 
         self.start_outbox_size = len(mail.outbox)
-        self.signup_response = self.client.post("/auth/signup/", {
+        client.post("/auth/signup/", {
             "first_name": "Василий",
             "last_name": "Пупкин",
             "username": "vasya2005",
@@ -18,14 +18,25 @@ class EmailTest(TestCase):
             "password2": "vasya12345678"
         }, follow=True)
 
-        self.user = User.objects.get(username="vasya2005")
-
     def test_send_email(self):
+        """Проверяет, что после регистрации пользователю отправилось письмо"""
         self.assertEqual(len(mail.outbox) - 1, self.start_outbox_size)
+
+
+class ProfileTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = User.objects.create_user(username="vasya2005", email="vasya@mail.ru", password="vasya")
 
     def test_profile_page(self):
         profile_response = self.client.get("/vasya2005/", follow=True)
         self.assertEqual(profile_response.status_code, 200)
+
+
+class CreatePostTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = User.objects.create_user(username="vasya2005", email="vasya@mail.ru", password="vasya")
 
     def test_publish_post(self):
         self.client.force_login(self.user)
@@ -51,12 +62,20 @@ class EmailTest(TestCase):
         post_response = self.client.get("/vasya2005/1/", follow=True)
         self.assertContains(post_response, "Очень интересный пост о жизни")
 
-    def test_edit_post(self):
+
+class EditPost(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = User.objects.create_user(username="vasya2005", email="vasya@mail.ru", password="vasya")
+        self.other_user = User.objects.create_user(username="fedor2004", email="fedor@yandex.ru", password="fedor")
+
+        # Добавим пост для пользователя vasya2005
         self.client.force_login(self.user)
         self.client.post("/new/", {
             "text": "Очень интересный пост о жизни",
         }, follow=True)
 
+    def test_edit_post(self):
         self.client.post("/vasya2005/1/edit/", {
             "text": "UPD: Пост стал ещё интереснее и жизненнее"
         }, follow=True)
@@ -69,3 +88,12 @@ class EmailTest(TestCase):
 
         post_response = self.client.get("/vasya2005/1/", follow=True)
         self.assertContains(post_response, "UPD: Пост стал ещё интереснее и жизненнее")
+
+    def test_edit_not_own_post(self):
+        self.client.force_login(self.other_user)
+
+        response = self.client.post("/vasya2005/1/edit/", {
+            "text": "Автора взломали"
+        }, follow=True)
+
+        self.assertIn(("/", 302), response.redirect_chain)
